aea482c8ee9476d3ee4d0eab2058d6673ccd1404
Heang Ly <heanglypa@gmail.com>
2023-01-09 08:28:00 -0500

    css

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -2,18 +2,19 @@ import { useActions } from '../hooks/use-actions'
 import './add-cell.css'
 
 interface AddCellProps {
-  nextCellId: string | null
+  forceVisible?: boolean
+  previousCellId: string | null
 }
 
-const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
-  const { insertCellBefore } = useActions()
+const AddCell: React.FC<AddCellProps> = ({ previousCellId, forceVisible }) => {
+  const { insertCellAfter } = useActions()
 
   return (
-    <div className='add-cell'>
+    <div className={`add-cell ${forceVisible && 'force-visible'}`}>
       <div className='add-buttons'>
         <button
           className='button is-rounded is-primary is-small'
-          onClick={() => insertCellBefore(nextCellId, 'code')}
+          onClick={() => insertCellAfter(previousCellId, 'code')}
         >
           <span className='icon is-small'>
             <i className='fas fa-plus' />
@@ -24,7 +25,7 @@ const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
 
         <button
           className='button is-rounded is-primary is-small'
-          onClick={() => insertCellBefore(nextCellId, 'text')}
+          onClick={() => insertCellAfter(previousCellId, 'text')}
         >
           <span className='icon is-small'>
             <i className='fas fa-plus' />
